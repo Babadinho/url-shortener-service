80181046d46404e1acbf222d48903064a08c9505
Donald Iloekwe <donytex4u@gmail.com>
2022-09-19 00:41:53 +0100

    shorturl-v1

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ fs.readdirSync('./routes').map((routes) =>
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build'));
   });
 }
 
