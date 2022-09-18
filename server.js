320514fc2dde1074b8396574e56c63d713e343f8
Donald Iloekwe <donytex4u@gmail.com>
2022-09-19 00:32:39 +0100

    shorturl-v1

--- server.js
+++ server.js
@@ -28,5 +28,12 @@ fs.readdirSync('./routes').map((routes) =>
   app.use('/api', require(`./routes/${routes}`))
 );
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
